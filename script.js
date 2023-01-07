05f3ad0119832a13bbc05b1e213545c2dacf5978
Eduardo Alvarado <ealvara73@gmail.com>
2023-01-07 13:56:46 -0600

    Create 16x16 div grid

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector('.container');
 
-for(let i =  0; i < 32; i++) {
-    let div = document.createElement('div');
-    div.setAttribute('class', 'square');
-    container.appendChild(div);
+for(let i =  0; i < 16; i++) {
+    let divRow = document.createElement('div');
+    divRow.setAttribute('class', 'row');
+    container.appendChild(divRow);
+
+    for(let z = 0; z < 16; z++) {
+        let divColumn = document.createElement('div');
+        divColumn.setAttribute('class', 'column');
+        divRow.appendChild(divColumn);
+    }
 }
